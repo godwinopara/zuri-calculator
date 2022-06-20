e86c149e9686c093cd7aa00a2d62eff2b5922a64
Godwin Opara <godwinopara62@yahoo.com>
2022-06-21 00:09:49 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+function multiplication(a, b){
+  return a * b;
+}
+
+function addition(a, b){
+  return a + b;
+}
+
+function subtraction(a, b){
+  return a - b;
+}
+
+function division(a, b){
+  return a / b;
+}
+
+
+
